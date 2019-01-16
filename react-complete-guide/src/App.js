7f1f60fcae38dbe5e245b7cc62716737359457a9
Sonia <sogispe@gmail.com>
2019-01-16 16:04:57 -0300

    Saving progress

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,12 +1,76 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = (newValue) => {
+    //console.log('button clicked!');
+    this.setState(
+      {
+        persons: [
+          { name: newValue, age: 28 },
+          { name: 'Manu', age: 29 },
+          { name: 'Stephanie', age: 37 }
+        ]
+      }
+    )
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState(
+      {
+        persons: [
+          { name: 'Max', age: 28 },
+          { name: event.target.value, age: 29 },
+          { name: 'Stephanie', age: 26 }
+        ]
+      }
+    )
+  }
+
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      paddig: '8px',
+      cursor: 'pointer'  
+    };
+
     return (
       <div className="App">
-        <h1>Testing react App</h1>
+        <h1>Hi! I'm React App!</h1>
+        <p>This is really working!</p>
+        <button
+          style={style} 
+          onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} 
+        />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age} 
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangedHandler}
+        >My hobby is: racing
+        </Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} 
+        />
       </div>
     );
   }
