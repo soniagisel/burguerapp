05df8db420a271bd0cc0f39d8a51a0b7fd5ab983
Sonia <sogispe@gmail.com>
2019-01-27 22:53:50 -0300

    Finished with module 4

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -7,23 +7,30 @@ class App extends Component {
 
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: 'abc', name: 'Max', age: 28 },
+      { id: 'def', name: 'Manu', age: 29 },
+      { id: 'ghi', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
   }
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+
+    const personIndex = this.state.persons.findIndex( p => {
+      return p.id === id;
+    });
+
+    const person = {...this.state.persons[personIndex]};
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = persons;
+
     this.setState(
-      {
-        persons: [
-          { name: 'Max', age: 28 },
-          { name: event.target.value, age: 29 },
-          { name: 'Stephanie', age: 26 }
-        ]
-      }
+      { persons: persons }
     )
   }
 
@@ -31,6 +38,20 @@ class App extends Component {
     const doesShow = this.state.showPersons;
     this.setState({showPersons: !doesShow});
   };
+
+  deletePersonHandler = (personIndex) => {
+    // slice creates a copy to not manipulate nor modify 
+    // the array directly, it's not a good practice.
+    // Another valid option would be:
+    // ---> const persons = [...this.state.persons];
+    const persons = this.state.persons.slice();
+    persons.splice(personIndex, 1);
+    this.setState(
+      {
+        persons: persons
+      }
+    );
+  }
 
   render() {
 
@@ -47,27 +68,15 @@ class App extends Component {
     if ( this.state.showPersons ) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
+          {this.state.persons.map((person, index) => {
             return <Person 
+              click={this.deletePersonHandler.bind(this, index)}
               name={person.name} 
               age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}
             />
           })}
-          <Person 
-            name={this.state.persons[0].name} 
-            age={this.state.persons[0].age} 
-          />
-          <Person 
-            name={this.state.persons[1].name} 
-            age={this.state.persons[1].age} 
-            click={this.switchNameHandler.bind(this, 'Max!')}
-            changed={this.nameChangedHandler}
-          >My hobby is: racing
-          </Person>
-          <Person 
-            name={this.state.persons[2].name} 
-            age={this.state.persons[2].age} 
-          />
         </div>
       )
     };
