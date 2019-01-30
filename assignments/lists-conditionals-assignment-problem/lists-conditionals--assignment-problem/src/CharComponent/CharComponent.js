6254185a63207f4e79e58c4ca681b68d6e9dbb16
Sonia <sogispe@gmail.com>
2019-01-30 15:43:17 -0300

    Finished fourth task

---
+++ assignments/lists-conditionals-assignment-problem/lists-conditionals--assignment-problem/src/CharComponent/CharComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const styles = {
+    display: 'inline-block', 
+    padding: '16px', 
+    textAlign: 'center', 
+    margin: '16px', 
+    border: '1px solid black'
+}
+
+const CharComponent = () => {
+    return <div style={styles} className="charComponent"></div>
+};
+
+export default CharComponent;
