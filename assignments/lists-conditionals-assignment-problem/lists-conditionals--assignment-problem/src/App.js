429deeb5d587aa30995bb504e4425d7a30e35a46
Sonia <sogispe@gmail.com>
2019-01-30 14:55:30 -0300

    Finished first task

--- assignments/lists-conditionals-assignment-problem/lists-conditionals--assignment-problem/src/App.js
+++ assignments/lists-conditionals-assignment-problem/lists-conditionals--assignment-problem/src/App.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    textLength: 0
+  }
+
+  outputLength = (event) => {
+    this.setState({
+      textLength: event.target.value.length
+    });
+  }
+
   render() {
     return (
       <div className="App">
+        <input type="text" onChange={(event) => this.outputLength(event)}/>
+        <p>Text length: {this.state.textLength}</p>
         <ol>
-          <li>Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
+          <li>DONE - Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
           <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
           <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
           <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
