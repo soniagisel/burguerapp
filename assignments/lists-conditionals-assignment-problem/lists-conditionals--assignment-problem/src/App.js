90300f52aee38bc5016b476c720c2fb7847271a4
Sonia <sogispe@gmail.com>
2019-01-30 15:08:50 -0300

    Finished second task

--- assignments/lists-conditionals-assignment-problem/lists-conditionals--assignment-problem/src/App.js
+++ assignments/lists-conditionals-assignment-problem/lists-conditionals--assignment-problem/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import ValidationComponent from './ValidationComponent/ValidationComponent';
 
 class App extends Component {
 
@@ -18,9 +19,10 @@ class App extends Component {
       <div className="App">
         <input type="text" onChange={(event) => this.outputLength(event)}/>
         <p>Text length: {this.state.textLength}</p>
+        <ValidationComponent textLength={this.state.textLength}/>
         <ol>
           <li>DONE - Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
-          <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
+          <li>DONE - Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
           <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
           <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
           <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
---
+++ assignments/lists-conditionals-assignment-problem/lists-conditionals--assignment-problem/src/ValidationComponent/ValidationComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ValidationComponent = (props) => {
+    return <p>{props.textLength}</p>
+}
+
+export default ValidationComponent;
