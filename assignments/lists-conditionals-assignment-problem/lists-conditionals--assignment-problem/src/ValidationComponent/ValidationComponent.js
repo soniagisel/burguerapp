f80c77be286fe60baaca1848e7d8f4c19225389d
Sonia <sogispe@gmail.com>
2019-01-30 15:29:52 -0300

    Finished third task

--- assignments/lists-conditionals-assignment-problem/lists-conditionals--assignment-problem/src/ValidationComponent/ValidationComponent.js
+++ assignments/lists-conditionals-assignment-problem/lists-conditionals--assignment-problem/src/ValidationComponent/ValidationComponent.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const ValidationComponent = (props) => {
-    return <p>{props.textLength}</p>
+    let msg = ""
+
+    if (props.textLength > 0 && props.textLength < 5) msg = "Text too short!"
+    if (props.textLength > 10) msg = "Text too long!"
+
+    return <div>
+                <p>{props.textLength}</p>
+                {msg}
+           </div>
 }
 
 export default ValidationComponent;
