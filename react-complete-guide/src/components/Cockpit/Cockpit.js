12745199758aaec9eb23d3b4d907543d6bf6bed1
Sonia <sogispe@gmail.com>
2019-02-21 23:37:38 -0300

    Uploading progress: useEffect

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
+
+    //componentDidMount and componentDidUpdate in one effect
+    //I can set as many useEffect as much as I want
+    useEffect(() => {
+        console.log('[Cockpit.js] useEffect');
+        // Http requests ...
+        setTimeout(() => {
+            alert('Saved data to cloud!');
+        }, 1000);
+    }, [props.persons]);
 
     const assignedClasses = [];
     let btnClass = '';
--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -26,16 +26,24 @@ class App extends Component {
     return state;
   };
 
-  componentWillMount() {
-    console.log('[App.js] componentWillMount');
-  }
+  // componentWillMount() {
+  //   console.log('[App.js] componentWillMount');
+  // }
 
   componentDidMount() {
     console.log('[App.js] componentDidMount');
   }
 
-  nameChangedHandler = (event, id) => {
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
 
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
+  }
+
+  nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex( p => {
       return p.id === id;
     });
