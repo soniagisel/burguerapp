9f2653c465baf91173f743befc4a100bda37e493
Sonia <sogispe@gmail.com>
2019-02-24 15:58:58 -0300

    Uploagind progress

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -34,10 +34,10 @@ const cockpit = (props) => {
         btnClass = classes.Red;
     }
     
-    if (props.persons.length <= 2) {
+    if (props.personsLength <= 2) {
       assignedClasses.push(classes.red);
     }
-    if (props.persons.length <= 1) {
+    if (props.personsLength <= 1) {
       assignedClasses.push(classes.bold);
     }
 
@@ -54,4 +54,4 @@ const cockpit = (props) => {
     );
 }
 
-export default cockpit;
+export default React.memo(cockpit);
