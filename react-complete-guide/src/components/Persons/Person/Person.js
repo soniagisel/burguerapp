0c493bc1a127f877488c4cc3a858c4548bc6f67a
Sonia <sogispe@gmail.com>
2019-02-25 21:26:03 -0300

    Uploagind progress

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,15 +1,18 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import classes from './Person.css';
+//import Aux from '../../../hoc/Auxiliary'; it's the same as React.Fragment
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <div className={classes.Person}>
-                <p onClick={this.props.click}>I am a {this.props.name} and I am {this.props.age} years old!</p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}/>
-            </div>
+            <Fragment>
+                <div className={classes.Person}>
+                    <p onClick={this.props.click}>I am a {this.props.name} and I am {this.props.age} years old!</p>
+                    <p>{this.props.children}</p>
+                    <input type="text" onChange={this.props.changed} value={this.props.name}/>
+                </div>
+            </Fragment>
         )
     }
 }
--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit'
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
 
@@ -105,7 +106,7 @@ class App extends Component {
 
     
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button onClick={() => {
           this.setState({showCockpit: false});
         }}>Remove Cockpit</button>
@@ -116,7 +117,7 @@ class App extends Component {
           clicked={this.togglePersonsHandler}
         />) : null}
         {persons}
-      </div>
+      </WithClass>
     );
   }
 }
